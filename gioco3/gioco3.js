f040e04a073c1d29f763989bda431729e872bf5d
turatistefanogalvani <turati.stefano@iisgalvanimi.edu.it>
2025-04-04 10:52:47 +0000

    collisioni

---
+++ gioco3/gioco3.js
@@ -0,0 +1,151 @@
+function startGame() {
+    myGamePiece.loadImages(running);
+    bushObject.loadImages();
+    crateObject.loadImages();
+    myGameArea.start();
+}
+
+var myGamePiece = {
+    speedX: 0,
+    speedY: 0,
+    width: 60,
+    height: 60,
+    x: 10,
+    y: 120,
+    imageList: [], // Array to store loaded images
+    contaFrame: 0, // Frame counter
+    actualFrame: 0, // Current frame to display
+    image: null, // Current image
+    tryX: 0,
+    tryY: 0,
+
+    update: function() {
+        this.tryY = this.y + this.speedY;
+        this.tryX = this.x + this.speedX;
+
+    //Prima di spostarmi realmente verifico che non ci siano collisioni
+        this.crashWith(bushObject);
+        this.contaFrame++;
+        if (this.contaFrame == 50) { // Change frame every 50 frames
+            this.contaFrame = 0;
+            this.actualFrame = (this.actualFrame + 1) % this.imageList.length;
+            this.image = this.imageList[this.actualFrame];
+        }
+    },
+
+    loadImages: function(running) {
+        for (let imgPath of running) {
+            var img = new Image();
+            img.src = imgPath;
+            this.imageList.push(img);
+        }
+        this.image = this.imageList[this.actualFrame];
+    },
+    crashWith: function(otherobj) {
+        var myleft = this.tryX;
+        var myright = this.tryX + this.width;
+        var mytop = this.tryY;
+        var mybottom = this.tryY + this.height;
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + otherobj.width;
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + otherobj.height;
+    
+        // Verifica collisione con l'altro oggetto
+        if ((mybottom >= othertop) && (mytop <= otherbottom) && (myright >= otherleft) && (myleft <= otherright)) {
+            return; // Collisione rilevata, non aggiornare la posizione
+        }
+    
+        // Verifica che non esca dal canvas
+        if (this.tryX < 0) this.tryX = 0; // Bordo sinistro
+        if (this.tryX + this.width > myGameArea.canvas.width) this.tryX = myGameArea.canvas.width - this.width; // Bordo destro
+        if (this.tryY < 0) this.tryY = 0; // Bordo superiore
+        if (this.tryY + this.height > myGameArea.canvas.height) this.tryY = myGameArea.canvas.height - this.height; // Bordo inferiore
+    
+        // Se non c'è collisione e non esce dai bordi, aggiorna la posizione
+        this.x = this.tryX;
+        this.y = this.tryY;
+    }
+};
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    context: null,
+    interval: null,
+
+    start: function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 1); // Update game every 20ms
+    },
+
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    drawGameObject: function (gameObject) {
+        this.context.drawImage(
+            gameObject.image,
+            gameObject.x,
+            gameObject.y,
+            gameObject.width,
+            gameObject.height
+        );
+    }
+};
+var bushObject = {
+    width: 100,
+    height: 50,
+    x: 100,
+    y: 270 - 50,
+    image : null,
+    loadImages: function() {
+        
+      this.image = new Image(this.width, this.height);
+      this.image.src = "https://i.ibb.co/CPdHYdB/Bush-1.png";
+    }
+  };
+
+var crateObject = {
+    width: 100,
+    height: 100,
+    x: 200,
+    y: 270 - 100,
+  
+    loadImages: function() {
+      this.image = new Image(this.width, this.height);
+      this.image.src = "https://i.ibb.co/GMgf32L/Crate.png";
+    }
+  };
+var running = ['spirite/Run1.png', 'spirite/Run2.png', 'spirite/Run3.png']; // Example paths for images
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.update();
+    myGameArea.drawGameObject(myGamePiece);
+    myGameArea.drawGameObject(bushObject);
+    myGameArea.drawGameObject(crateObject);
+}
+
+// Control functions
+function moveup() {
+    myGamePiece.speedY -= 0.4; 
+}
+
+function movedown() {
+    myGamePiece.speedY += 0.4; 
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 0.4; 
+}
+
+function moveright() {
+    myGamePiece.speedX += 0.4; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
---
+++ gioco3/spirite1.js
@@ -0,0 +1,12 @@
+var running = [
+    "spirite/Run1.png",
+    "spirite/Run2.png",
+    "spirite/Run3.png",
+    "spirite/Run4.png",
+    "spirite/Run5.png",
+    "spirite/Run6.png",
+    "spirite/Run7.png",
+    "spirite/Run8.png",
+    "spirite/Run9.png",
+    "spirite/Run10.png"
+];
